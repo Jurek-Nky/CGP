0d2c480ecec47e691b2b024d9ee6bf565c9d6d50
jurek-nky <jurek.nit@gmail.com>
2022-11-07 10:35:07 +0100

    added all config properties for all ui components; initialized default config props

--- gui/config.cpp
+++ gui/config.cpp
@@ -8,5 +8,32 @@ float Config::animationSpeed = 1.0f;
 
 float Config::camZoom = 5.0f;
 
-bool Config::localRotation = true;
+bool Config::localRotationEnable = true;
+bool Config::globalRotationEnable = true;
+bool Config::gridEnable = true;
+bool Config::lightingEnable = true;
+bool Config::orbitEnable = true;
 
+bool Config::deathStarActive = true;
+bool Config::deathStarPreviewEnable = true;
+float Config::deathStarLaserLen = 1.0;
+float Config::deathStarLaserRad = 1.0;
+
+bool Config::planetPathEnable[] = {false,       // earth
+                                   false,       // moon
+                                   false,       // sun
+                                   false,       // mercury
+                                   false,       // venus
+                                   false,       // mars
+                                   false,       // jupiter
+                                   false,       // saturn
+                                   false,       // io
+                                   false,       // europa
+                                   false,       // ganymede
+                                   false        // callisto
+};
+
+
+float Config::resolutionU = 1.0;
+float Config::resolutionV = 1.0;
+float Config::resolutionRing = 1.0;
