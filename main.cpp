0fde7845bc5573ecb203741afcc0079317b3d56a
jurek-nky <jurek.nit@gmail.com>
2022-11-18 18:26:11 +0100

    reworked stylesheet import

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <QFile>
 #include <iostream>
 #include <QDir>
+#include <QTextStream>
 
 #include "gui/glwidget.hpp"
 #include "gui/mainwindow.hpp"
@@ -10,14 +11,11 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     // stylesheet
-    QFile file("./style/style.css");
-    file.open(QFile::ReadOnly);
-    if (file.exists()) {
-        QString styleSheet = QLatin1String(file.readAll());
-        app.setStyleSheet(styleSheet);
-    } else {
-        std::cerr << "stylesheet not found." << std::endl;
-    }
+    QFile f(":/style/style.css");
+    if (!f.open(QFile::ReadOnly | QFile::Text))
+        std::cout << "Could not open file " << ":/style/style.css" << std::endl;
+    QTextStream in(&f);
+    app.setStyleSheet(in.readAll());
 
     // set gl format
     GLWidget::setGLFormat();
